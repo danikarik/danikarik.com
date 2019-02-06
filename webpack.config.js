dde47985d4320d74def3c6d5d2f2b0af14d07ef2
danikarik <baitursynov92@gmail.com>
2019-02-06 22:32:29 +0600

    add tailwindcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,60 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: {
-        app: './src/app.js'
-    },
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.html'
+  mode: process.env.NODE_ENV,
+  entry: {
+    app: ['./src/js/app.js', './src/css/app.css']
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader',
+          ],
         }),
-        new ManifestPlugin(),
-        new webpack.HashedModuleIdsPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    output: {
-        filename: '[name].[hash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
-                }
-            }
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new ExtractTextPlugin('app.css', {
+      disable: process.env.NODE_ENV === 'development'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new ManifestPlugin(),
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
         }
+      }
     }
+  }
 };
